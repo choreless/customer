dbbfc90462c21218bde264044bfc4ee691a0d6f4
imithu <imithu@imithu.org>
2023-12-28 16:59:04 +0600

    🔧 add - tailwind.config.ts

---
+++ tailwind.config.ts
@@ -0,0 +1,29 @@
+import daisyui from 'daisyui';
+import type { Config } from 'tailwindcss';
+
+const config: Config = {
+	content: [
+		"./components/**/*.{js,vue,ts}",
+		"./layouts/**/*.vue",
+		"./pages/**/*.vue",
+		"./plugins/**/*.{js,ts}",
+		"./app.vue",
+		"./error.vue",
+	],
+	theme: {
+		extend: {
+			colors: {
+				brand: {
+					black: '#011631',
+					blue: '#3063ff',
+					orange: '#f85a47',
+				}
+			}
+		}
+	},
+	plugins: [daisyui],
+	daisyui: {
+		themes: ['light']
+	}
+}
+export default config;

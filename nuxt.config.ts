de39550e3aab2ff10e9f388f23b7795c884835d8
imithu <imithu@imithu.org>
2023-12-28 17:00:56 +0600

    🔧 add - tailwindcss in nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -8,6 +8,13 @@ const ApiMain = process.env.ApiMain ?? 'https://api.getchoreless.com';
 const debug = Boolean(process.env.debug) ?? false;
 
 export default defineNuxtConfig({
+	modules: ['@nuxtjs/tailwindcss'],
+
+	tailwindcss: {
+		viewer: false,
+		cssPath: '~/assets/css/index.css'
+	},
+
 	runtimeConfig: {
 		public: {
 			base: {

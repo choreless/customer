2a317aba323494537f4a05dc83d9b41312bdf41d
imithu <imithu@imithu.org>
2023-12-29 03:17:47 +0600

    🌱 add - global loading

--- app.vue
+++ app.vue
@@ -1,8 +1,15 @@
+<script setup lang="ts">
+const setting = useSetting();
+</script>
+
 <template>
 <div>
 	<NuxtLayout>
 		<NuxtPage />
 	</NuxtLayout>
+	<div v-if="setting.loading" class='fixed top-0 w-full h-full flex items-center justify-center bg-black/30'>
+		<span class='loading loading-infinity w-20 bg-gradient-to-r from-blue-500 to-green-500'></span>
+	</div>
 	<notifications position="bottom right" :duration="2000" :pause-on-hover="true" width="100%" />
 </div>
 </template>

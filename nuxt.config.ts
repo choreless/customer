7d6f279630450eaa5c9a0e70cb248cf207380fab
imithu <imithu@imithu.org>
2023-12-28 15:24:54 +0600

    🔧 add - various config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,25 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
+/** base */
+const AppName = 'Admin';
+const AppUrl  = process.env.AppUrl ?? 'http://localhost:3000';
+const AppVersion = 'v0.1.0';
+const ApiMain = process.env.ApiMain ?? 'https://api.getchoreless.com';
+
+/** dev */
+const debug = Boolean(process.env.debug) ?? false;
+
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+	runtimeConfig: {
+		public: {
+			base: {
+				AppName,
+				AppUrl,
+				AppVersion,
+				ApiMain
+			},
+			dev: {
+				debug
+			}
+		}
+	},
+	devtools: { enabled: debug }
 })

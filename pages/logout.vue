d8bc32836f6ffaf73e258ff856190f7d499cf28b
imithu <imithu@imithu.org>
2023-12-29 23:06:04 +0600

    🌱 add - logout

---
+++ pages/logout.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+useHead({ title: 'Logout' })
+definePageMeta({ middleware: 'auth' })
+const api = useApi();
+const setting = useSetting();
+const user = useUser();
+
+setting.loading = true;
+await api.delete('/users/logout');
+user.$reset();
+setting.loading = false;
+await navigateTo('/');
+</script>
+
+<template>
+<div>
+</div>
+</template>

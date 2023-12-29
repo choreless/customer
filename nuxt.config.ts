829fc16889c08bc020a425830bc42ee58e5fc354
imithu <imithu@imithu.org>
2023-12-29 17:59:53 +0600

    🔧 add - nuxt-icon in nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -8,7 +8,7 @@ const ApiMain = process.env.ApiMain ?? 'https://api.getchoreless.com';
 const debug = Boolean(process.env.debug) ?? false;
 
 export default defineNuxtConfig({
-	modules: ['@nuxtjs/tailwindcss', '@pinia/nuxt'],
+	modules: ['@nuxtjs/tailwindcss', '@pinia/nuxt', 'nuxt-icon'],
 
 	tailwindcss: {
 		viewer: false,

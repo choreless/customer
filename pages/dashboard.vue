d0faf594ac074b7112b84cdec20a2f4e7de994fe
imithu <imithu@imithu.org>
2024-01-01 19:07:05 +0600

    🌱 add - dashboard page

---
+++ pages/dashboard.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import AdminPanel from '~/components/dashboard/AdminPanel.vue';
+
+useHead({ title: 'Dashboard' })
+definePageMeta({ middleware: 'auth' })
+const {usertype} = useUser();
+</script>
+
+<template>
+<div>
+	<AdminPanel v-if="usertype==='admin'" />
+</div>
+</template>

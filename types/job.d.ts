13f11d4ca7daa80c4a703394710287b2348f27e9
imithu <imithu@imithu.org>
2024-01-01 19:06:32 +0600

    🏷️ add - JobAdmin in job type

--- types/job.d.ts
+++ types/job.d.ts
@@ -11,4 +11,17 @@ interface JobWasher {
 	created_at: string
 }
 
-export { JobStatus, JobWasher }
+interface JobAdmin {
+	id: string
+	cid: string
+	customer_first_name: string
+	customer_last_name: string
+	washer_first_name: string
+	washer_last_name: string
+	bags: number[]
+	stopwatch: number
+	created_at: string
+}
+
+
+export { JobStatus, JobWasher, JobAdmin }

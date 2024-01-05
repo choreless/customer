10dc9461d761830ee57de4b8d9f8d339a77d6b1b
imithu <imithu@imithu.org>
2024-01-05 17:01:38 +0600

    🌱 add - customers link in dashboard

--- components/dashboard/AdminPanel.vue
+++ components/dashboard/AdminPanel.vue
@@ -4,6 +4,7 @@ import CompletedJobs from '~/components/jobs/admin/Completed.vue';
 
 <template>
 <div class="max-w-4xl mx-auto px-4">
+	<NuxtLink href="/customers" class="btn btn-sm btn-outline w-fit mt-6 btn-primary">Customers</NuxtLink>
 	<div class="mt-6">
 		<h1 class="flex items-center gap-x-2 text-2xl font-mono font-bold">Completed jobs</h1>
 		<CompletedJobs />

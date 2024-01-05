49c820ab8d399bd8f1f86f27961f3423fc24aa23
imithu <imithu@imithu.org>
2024-01-05 16:53:01 +0600

    🌱 add - customer lib

---
+++ lib/customer.ts
@@ -0,0 +1,4 @@
+export const addons = ['Delicates', 'Hang-Dry', 'Hangers', "Don't mix bags", 'Fabric Softener', 'OxiClean (Whites Only)'] as const;
+export const detergents = ['Premium Scented', 'Hypoallergenic', 'I will provide'] as const;
+export const dryer_temperatures = ['High', 'Medium', 'Low'] as const;
+export const water_temperatures = ['Cold', 'Hot'] as const;

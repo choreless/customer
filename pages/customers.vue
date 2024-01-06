d7d58afad972a09832753aaf27c2401dd1a4eb5b
imithu <imithu@imithu.org>
2024-01-06 07:32:33 +0600

    🌱 add - Show component

--- pages/customers.vue
+++ pages/customers.vue
@@ -2,6 +2,7 @@
 import Index from '~/components/customers/admin/Index.vue';
 import Create from '~/components/customers/admin/Create.vue';
 import EmptyPanel from '~/components/customers/admin/EmptyPanel.vue';
+import Show from '~/components/customers/admin/Show.vue';
 
 useHead({ title: 'Customers' })
 definePageMeta({ middleware: 'admin' })
@@ -19,8 +20,9 @@ onMounted(()=>{
 	<div class="max-w-7xl w-full mx-auto mt-4 grid grid-cols-1 sm:grid-cols-2">
 		<Index :class="customer.end_panel ? 'hidden sm:block' : 'block'" />
 		<div :class="customer.end_panel ? 'block' : 'hidden sm:block'" >
-			<Create v-if="customer.end_panel==='create'" class="w-full" />
 			<EmptyPanel v-show="customer.end_panel===undefined" class="w-full" />
+			<Show v-show="customer.end_panel==='show'" class="w-full" />
+			<Create v-if="customer.end_panel==='create'" class="w-full" />
 		</div>
 	</div>
 </section>

e555967943dea32c75b4cdda956518b6099daf8f
imithu <imithu@imithu.org>
2024-01-11 00:03:28 +0600

    🔧 add - .eslintrc.cjs

---
+++ .eslintrc.cjs
@@ -0,0 +1,26 @@
+module.exports = {
+	root: true,
+	extends: ['@nuxtjs/eslint-config-typescript'],
+	rules: {
+		'arrow-parens': ['warn', 'as-needed'],
+		'arrow-spacing': ['off'],
+		'block-spacing': ['off'],
+		'brace-style': ['off'],
+		'import/no-named-as-default': ['off'],
+		'keyword-spacing': ['off'],
+		'no-tabs': ['warn', { allowIndentationTabs: true }],
+		'object-curly-spacing': ['off'],
+		'object-property-newline': ['off'],
+		'space-before-blocks': ['off'],
+		'space-before-function-paren': ['off', 'never'],
+		'space-infix-ops': ['off'],
+		'vue/html-indent': ['warn', 'tab', { baseIndent: 0 }],
+		'vue/max-attributes-per-line': ['off'],
+		'vue/multi-word-component-names': ['off'],
+		'vue/singleline-html-element-content-newline': ['off'],
+		camelcase: ['off'],
+		curly: ['off'],
+		indent: ['warn', 'tab'],
+		semi: ['off']
+	}
+};

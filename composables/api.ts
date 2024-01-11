95a7c0c15fb39277a4d51b82225b441597878306
imithu <imithu@imithu.org>
2024-01-11 13:24:09 +0600

    🌱 add - usertype in common headers

--- composables/api.ts
+++ composables/api.ts
@@ -2,8 +2,9 @@ import axios from 'axios';
 
 export const useApi = ()=>{
 	const config = useRuntimeConfig();
-	const {token} = useUser();
+	const {token, usertype} = useUser();
 	axios.defaults.baseURL = config.public.base.ApiMain;
 	if(token) axios.defaults.headers.common['User-Authorization'] = token;
+	if(usertype) axios.defaults.headers.common['User-Usertype'] = usertype;
 	return axios;
 }

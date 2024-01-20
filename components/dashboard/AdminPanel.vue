0d18fe72e97b996496910206a894fca5f43eedac
imithu <imithu@imithu.org>
2024-01-20 13:32:57 +0600

    🔥 remove - completed jobs

--- components/dashboard/AdminPanel.vue
+++ components/dashboard/AdminPanel.vue
@@ -1,13 +1,5 @@
-<script setup lang="ts">
-import CompletedJobs from '~/components/jobs/admin/Completed.vue';
-</script>
-
 <template>
 <div class="max-w-4xl mx-auto px-4">
 	<NuxtLink href="/customers" class="btn btn-sm btn-outline w-fit mt-6 btn-primary">Customers</NuxtLink>
-	<div class="mt-6">
-		<h1 class="flex items-center gap-x-2 text-2xl font-mono font-bold">Completed jobs</h1>
-		<CompletedJobs />
-	</div>
 </div>
 </template>

d187b61327954308c568527176035955afec10dd
imithu <imithu@imithu.org>
2024-01-26 02:37:53 +0600

    🌱 add - update ui

--- app.vue
+++ app.vue
@@ -1,10 +1,18 @@
 <script setup lang="ts">
 const setting = useSetting();
+const { $pwa } = useNuxtApp();
 </script>
 
 <template>
 <div>
 	<VitePwaManifest />
+	<div v-show="$pwa?.needRefresh" class="fixed w-full h-full z-20 flex flex-col items-center justify-center gap-y-2 bg-white">
+		<button class="btn btn-outline btn-primary text-white" @click="$pwa?.updateServiceWorker()">
+			<Icon name="material-symbols:deployed-code-update-outline-sharp" class="text-xl" />
+			<p>Update</p>
+		</button>
+		<h1 class="text-xl">Update is available.</h1>
+	</div>
 	<NuxtLayout>
 		<NuxtPage />
 	</NuxtLayout>

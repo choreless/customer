d765f17343eaf666d9ad57e35a3de80707e93dbb
imithu <imithu@imithu.org>
2024-01-25 12:23:12 +0600

    🌱 add - due text

--- pages/orders/active.vue
+++ pages/orders/active.vue
@@ -81,7 +81,7 @@ watch([search, sort, status], ()=>{getOrders();})
 				<tr v-for="order of orders" :key="order.id" class="bg-neutral-200/60 [&_td]:border-black/5 shadow-[0px_0px_4px_0px_rgba(0,_0,_0,_0.05)]">
 					<td class="border border-e-0 text-lg font-medium uppercase">{{ order.id }}</td>
 					<td class="border-y text-base">{{ order.customer_name }}</td>
-					<td class="border-y font-medium" :class="differenceInSeconds(new Date(order.due_time), now)<0 && 'text-brand-orange'">{{ format(order.due_time as any as Date, 'EEE M/dd, hh:mm aaa') }}</td>
+					<td class="border-y font-medium" :class="differenceInSeconds(new Date(order.due_time), now)<0 && 'text-[#c46040]'">Due: {{ format(order.due_time as any as Date, 'EEE M/dd, hh:mm aaa') }}</td>
 					<td class="border-y">
 						<div class="flex items-center gap-x-1.5">
 							<img src="https://ik.imagekit.io/choreless/v2/icons/bag2.svg" alt="icon" loading="lazy" class="w-4">

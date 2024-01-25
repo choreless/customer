44a75479257b249c386cb3a9f1c725b3bdc71226
imithu <imithu@imithu.org>
2024-01-25 14:48:10 +0600

    💄 add - maximum height

--- components/customers/admin/Index.vue
+++ components/customers/admin/Index.vue
@@ -21,7 +21,7 @@ const customer_store = useAdminCustomer();
 			<p>loading</p>
 		</div>
 		<p class="px-2">{{ customer_store.customers.length }} customer{{ customer_store.customers.length>1 ? 's':'' }}</p>
-		<div v-if="customer_store.customers.length">
+		<div v-if="customer_store.customers.length" class="min-h-[9rem] max-h-[calc(100vh-17rem)] overflow-y-auto">
 			<button v-for="customer of customer_store.customers" :key="customer.id" class="flex items-center justify-between gap-x-3 w-full py-2.5 px-4 text-start border-b [&.active]:bg-brand-black/10 hover:!bg-brand-black/10" :class="customer_store.id===customer.id && 'active'" @click="customer_store.id=customer.id">
 				<div class="whitespace-nowrap overflow-auto">
 					<p>{{ customer.first_name }} {{ customer.last_name }}</p>
--- pages/customers.vue
+++ pages/customers.vue
@@ -19,7 +19,7 @@ onMounted(()=>{
 <section class="mx-2.5 mb-4">
 	<div class="max-w-7xl w-full mx-auto mt-4 grid grid-cols-1 sm:grid-cols-2">
 		<Index :class="customer.end_panel ? 'hidden sm:block' : 'block'" />
-		<div :class="customer.end_panel ? 'block' : 'hidden sm:block'">
+		<div class="min-h-[20rem] max-h-[calc(100vh-6rem)] overflow-y-auto" :class="customer.end_panel ? 'block' : 'hidden sm:block'">
 			<EmptyPanel v-show="customer.end_panel===undefined" class="w-full" />
 			<Show v-show="customer.end_panel==='show'" class="w-full" />
 			<Create v-if="customer.end_panel==='create'" class="w-full" />

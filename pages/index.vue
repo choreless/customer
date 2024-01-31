9cb53741ec5ef900d8d3fadf7ba6a6c74013c163
imithu <imithu@imithu.org>
2024-01-31 12:39:54 +0600

    🌱 add - login with phone and password

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { useIMask } from 'vue-imask';
 import type { Role } from '@/types/user';
+type LoginResponse = false|{roles: Role[], token: string}
 
 useHead({ title: 'Login' })
 definePageMeta({
@@ -11,18 +12,20 @@ const setting = useSetting();
 const user = useUser();
 const api = useApi();
 
-const step = ref<'phone'|'otp'>('phone');
+const login_method = ref<'phone_otp'|'phone_password'>('phone_otp');
 const phone = reactive({masked: '', unmasked: ''});
-const uid = ref('');
+const phone_otp_step = ref<'phone'|'otp'>('phone');
 const otp = ref('');
-const loading = ref(false);
+const password = ref('');
+const show_password = ref(false);
 const error = reactive({
-	otp: false,
 	phone: undefined as undefined | boolean,
-	does_account_exist: true
+	invalid_credential: false
 })
 
 function phone_paste(e: ClipboardEvent){
+	const selection = getSelection()?.toString()?.replace(/[^\d]+/g, '');
+	if(selection && selection.length===10) phone_imask.unmasked.value = '';
 	if(phone_imask.unmasked.value.length!==0) return false;
 	let phone = e.clipboardData?.getData('text/plain');
 	if(phone){
@@ -32,61 +35,53 @@ function phone_paste(e: ClipboardEvent){
 	}
 }
 
-async function sendOtp(){
-	if(phone.unmasked.length===10){
-		setting.loading = true;
-		loading.value = true;
-		error.phone = false;
-		const usp = new URLSearchParams();
-		usp.append('step', 'send_otp');
-		usp.append('phone', `+1${phone.unmasked}`);
-		const {data} = await api.get(`/users/login?${usp}`);
-		if(data){
-			notify.info('A code has been sent.');
-			step.value = 'otp';
-			uid.value = data;
-		}
-		else error.does_account_exist = false;
-		loading.value = false;
-		setting.loading = false;
-	}
-	else error.phone = true;
-}
-
-function resendOtp(){
-	sendOtp();
-	otp.value = '';
-}
-
-async function verifyOtp(){
-	setting.loading = true;
-	loading.value = true;
-	const usp = new URLSearchParams();
-	usp.append('step', 'verify_otp');
-	usp.append('uid', uid.value);
-	usp.append('otp', otp.value);
-	const {data}: {data: false|{roles: Role[], token: string}} = await api.get(`/users/login?${usp}`);
+async function login(usp: URLSearchParams){
+	const {data}: {data: LoginResponse} = await api.get(`/users/login?${usp}`);
 	if(data){
-		error.otp = false;
 		user.token = data.token;
 		user.roles = data.roles;
 		user.usertype = data.roles[0];
 		await navigateTo('/dashboard');
 		notify.success('Login is successful');
 	}
-	else error.otp = true;
-	loading.value = false;
+	else error.invalid_credential = true;
+}
+
+async function verify(){
+	error.invalid_credential = false;
+	error.phone = phone.unmasked.length!==10;
+	if(error.phone) return false;
+	setting.loading = true;
+	if(login_method.value==='phone_otp' && phone_otp_step.value==='phone'){
+		const usp = new URLSearchParams();
+		usp.append('login_method', 'phone_otp');
+		usp.append('step', 'send_otp');
+		usp.append('phone', `+1${phone.unmasked}`);
+		await api.get(`/users/login?${usp}`);
+		notify.info('A code has been sent.');
+		phone_otp_step.value = 'otp';
+	}
+	else {
+		const usp = new URLSearchParams();
+		usp.append('phone', `+1${phone.unmasked}`);
+		usp.append('login_method', login_method.value);
+		if(login_method.value==='phone_password') usp.append('password', password.value);
+		else {
+			usp.append('step', 'verify_otp');
+			usp.append('otp', otp.value);
+		}
+		await login(usp);
+	}
 	setting.loading = false;
 }
 
-watch(otp, n=>{ n.length===6 ? verifyOtp() : error.otp = false; })
+watch(otp, n=>{ n.length===6 ? verify() : error.invalid_credential = false; })
 const phone_imask = useIMask({
 	mask: [{mask: '000'}, {mask: '(000) 000-0000'}]
 }, {
 	onAccept(){
-		step.value = 'phone';
+		phone_otp_step.value = 'phone';
 		error.phone = undefined;
-		error.does_account_exist = true;
 		phone.masked=phone_imask.masked.value;
 		phone.unmasked=phone_imask.unmasked.value;
 	}
@@ -96,11 +91,11 @@ const phone_imask = useIMask({
 <template>
 <div class="fixed left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 max-w-[22rem] w-full px-4">
 	<h1 class="text-brand-black text-center text-5xl leading-[3.5rem] font-bold">Hello there!</h1>
-	<p class="text-brand-black/50 text-center leading-4 mt-2.5 text-sm sm:text-base">Enter your phone number below to Login.</p>
+	<p class="text-brand-black/50 text-center leading-4 mt-2.5 text-sm sm:text-base">Enter your phone number <template v-if="login_method==='phone_password'">and password </template>below to Login.</p>
 	<div class="mt-6">
 		<div class="grid grid-cols-[1fr_auto] justify-around items-center gap-x-2 border border-base-content/20 rounded-[0.3125rem]">
 			<label class="input-float">
-				<input :ref="phone_imask.el" type="tel" class="!border-none" :value="phone.masked" placeholder="" @paste="phone_paste" @keydown.enter="sendOtp()">
+				<input :ref="phone_imask.el" type="tel" class="!border-none" :value="phone.masked" placeholder="" @paste="phone_paste" @keydown.enter="verify()">
 				<p>Phone number</p>
 			</label>
 			<div class="w-6 h-6 me-4">
@@ -110,23 +105,39 @@ const phone_imask = useIMask({
 			</div>
 		</div>
 		<p v-if="error.phone" class="text-error">* Invalid phone number</p>
-		<p v-if="!error.does_account_exist" class="text-error">* Account does not exists</p>
 	</div>
-	<div class="mt-6">
-		<button v-if="step==='phone' && !loading" class="btn w-full h-[3.75rem] rounded-[0.3125rem] text-2xl px-5 py-[0.9375rem] text-white bg-brand-orange border-brand-orange hover:text-brand-orange hover:bg-transparent hover:border-brand-orange" @click="sendOtp()">Verify</button>
-		<div v-if="step==='otp'">
+	<div v-if="login_method==='phone_password'" class="mt-3">
+		<div class="grid grid-cols-[1fr_auto] justify-around items-center px-0">
+			<label class="input-float w-full h-full">
+				<input v-model="password" :type="show_password ? 'text':'password'" placeholder="" class="!border-r-0 !rounded-r-none" @keydown.enter="verify()">
+				<p>Password</p>
+			</label>
+			<div class="input border-base-content/20 rounded-[0.3125rem] !outline-none h-[3.75rem] rounded-s-none border-s-0 flex items-center ps-0">
+				<button class="btn btn-sm btn-square bg-transparent border-none shadow-none text-[#727e8c]/50 hover:bg-brand-orange hover:text-white" @click="show_password=!show_password">
+					<Icon :name="show_password ? 'zondicons:view-show' : 'zondicons:view-hide'" class="text-2xl" />
+				</button>
+			</div>
+		</div>
+	</div>
+	<div class="mt-3">
+		<p v-if="login_method==='phone_password' && error.invalid_credential" class="text-error">* Invalid credential</p>
+		<button v-if="!setting.loading && (login_method==='phone_password' || (login_method==='phone_otp' && phone_otp_step==='phone'))" class="btn w-full h-[3.75rem] rounded-[0.3125rem] text-2xl px-5 py-[0.9375rem] text-white bg-brand-orange border-brand-orange hover:text-brand-orange hover:bg-transparent hover:border-brand-orange" @click="verify()">Verify</button>
+		<div v-if="login_method==='phone_otp' && phone_otp_step==='otp'">
 			<div class="grid grid-cols-[1fr_auto] justify-around items-center px-0">
 				<label class="input-float w-full h-full">
 					<input v-model="otp" type="text" inputmode="numeric" maxlength="6" placeholder="" class="!border-r-0 !rounded-r-none">
 					<p>Code</p>
 				</label>
 				<div class="input border-base-content/20 rounded-[0.3125rem] !outline-none h-[3.75rem] rounded-s-none border-s-0 flex items-center">
-					<button :disabled="loading" class="btn btn-sm bg-transparent border-none shadow-none hover:bg-brand-orange hover:text-white uppercase" @click="resendOtp()">RESEND!</button>
+					<button :disabled="setting.loading" class="btn btn-sm bg-transparent border-none shadow-none hover:bg-brand-orange hover:text-white uppercase" @click="phone_otp_step='phone';otp='';verify()">RESEND!</button>
 				</div>
 			</div>
-			<p v-if="error.otp" class="text-error">* Invalid OTP</p>
+			<p v-if="error.invalid_credential" class="text-error">* Invalid OTP</p>
 		</div>
 	</div>
-	<p v-if="loading" class="flex items-center justify-center gap-x-2 text-xl font-bold mt-6 text-brand-orange">Verifying <span class="loading loading-bars" /></p>
+	<button class="flex items-center gap-x-2 ms-auto mt-2 group" @click="login_method=login_method==='phone_otp' ? 'phone_password' : 'phone_otp'; error.invalid_credential = false;">
+		<p class="group-hover:text-info">Login with:</p>
+		<div class="btn btn-sm btn-outline btn-info group-hover:border-0 group-hover:bg-info group-hover:text-black" :class="login_method==='phone_otp' ? 'uppercase' : 'capitalize'">{{ login_method.slice(6) }}</div>
+	</button>
 </div>
 </template>

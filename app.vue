13f8af772ea1d30ad6986ddde8565267f118ea4e
imithu <imithu@imithu.org>
2024-02-02 19:35:34 +0600

    🌱 add - loading indicator

--- app.vue
+++ app.vue
@@ -3,6 +3,7 @@ import Update from '~/components/app/Update.vue';
 
 const setting = useSetting();
 const { $pwa } = useNuxtApp();
+const { isLoading, progress } = useLoadingIndicator();
 </script>
 
 <template>
@@ -16,5 +17,6 @@ const { $pwa } = useNuxtApp();
 		<span class="loading loading-infinity w-20 bg-gradient-to-r from-blue-500 to-green-500" />
 	</div>
 	<notifications position="bottom right" :duration="2000" :pause-on-hover="true" width="100%" />
+	<p v-show="isLoading" class="fixed bottom-0 text-end text-xs font-bold rounded-r-lg pe-1.5 duration-200 bg-brand-orange text-white" :style="{width: `${progress.toFixed(0)}%` }">{{ progress.toFixed(0) }}%</p>
 </div>
 </template>

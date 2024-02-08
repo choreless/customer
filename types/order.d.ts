6d5f50f0cdacfec53e8e5a99390ff0c0cd4ff55b
imithu <imithu@imithu.org>
2024-02-09 01:14:49 +0600

    🏷️ add - Orders4Washer interface

--- types/order.d.ts
+++ types/order.d.ts
@@ -8,3 +8,15 @@ export interface Order4Washer {
 	due_time: string|null
 	customer_id: string
 }
+
+export interface Orders4Washer {
+	id: string
+	bags_initial: Bags,
+	is_wet: boolean,
+	bags_final: Bags,
+	due_time: string|null,
+	completed_at: string|null,
+	status: OrderStatus
+	created_at: string
+	customer_name: string
+}

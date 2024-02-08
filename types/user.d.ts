7c095fe611def6d283473e59ad65644dfd092601
imithu <imithu@imithu.org>
2024-02-09 01:08:50 +0600

    🏷️ add - CustomerInfo4Washer

--- types/user.d.ts
+++ types/user.d.ts
@@ -1,4 +1,11 @@
-type Role = 'admin' | 'customer' | 'driver' | 'washer' | '';
-type CustomerType = 'retail' | 'commercial';
+export type Role = 'admin' | 'customer' | 'driver' | 'washer' | '';
+export type CustomerType = 'retail' | 'commercial';
 
-export { Role, CustomerType }
+export interface CustomerInfo4Washer {
+	detergent: string
+	water_temperature: string
+	dryer_temperature: string
+	addons: string[]
+	comment: string|null
+	customer_name: string
+}

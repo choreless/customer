e7582473bb2ededebec4bf8de3d109d3551bad69
imithu <imithu@imithu.org>
2024-02-09 01:14:13 +0600

    🏷️ add - Order4Washer interface

--- types/order.d.ts
+++ types/order.d.ts
@@ -1,1 +1,10 @@
+type Bags = null|number[];
 export type OrderStatus = 'in_progress'|'complete';
+
+export interface Order4Washer {
+	bags_initial: Bags
+	is_wet: boolean
+	bags_final: Bags
+	due_time: string|null
+	customer_id: string
+}

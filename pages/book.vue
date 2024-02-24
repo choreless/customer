8ab9709fa35230d60b505a1a9d1fef712b9e72ca
imithu <imithu@imithu.org>
2024-02-25 01:16:31 +0600

    🌱 add - book page

---
+++ pages/book.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import Header from '@/components/book/Header.vue';
+import SelectServices from '@/components/book/SelectServices.vue';
+import CarePreferences from '@/components/book/CarePreferences.vue';
+import SelectPickupInfo from '@/components/book/SelectPickupInfo.vue';
+
+useHead({ title: 'Book' })
+definePageMeta({
+	layout: 'clean',
+	middleware: 'guest'
+})
+
+const book = usePageBook();
+</script>
+
+<template>
+<div>
+	<Header />
+	<SelectServices v-if="book.step===0" />
+	<CarePreferences v-else-if="book.step===1" />
+	<SelectPickupInfo v-else-if="book.step===2" />
+</div>
+</template>

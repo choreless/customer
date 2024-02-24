62df68076d3446508645e27d873a209a208c1c46
imithu <imithu@imithu.org>
2024-02-24 21:26:27 +0600

    🌱 add - Close icon

---
+++ components/icon/Close.vue
@@ -0,0 +1,5 @@
+<template>
+<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" stroke="black">
+	<path d="M22 2L2 22M2.00002 2L22 22" strokeWidth="3.5" strokeLinecap="round" strokeLinejoin="round" />
+</svg>
+</template>

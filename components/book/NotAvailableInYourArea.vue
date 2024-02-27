b481f504f082e3819a0e3fb7d1208f66af27e099
imithu <imithu@imithu.org>
2024-02-27 15:28:57 -0800

    🌱 add - NotAvailableInYourArea component

---
+++ components/book/NotAvailableInYourArea.vue
@@ -0,0 +1,8 @@
+<template>
+<div class="h-screen grid items-center justify-center content-center bg-[linear-gradient(180deg,_#ffdbd7_0%,_rgba(255,_219,_215,_0.00)_100%)]">
+	<p class="text-center text-8xl leading-[.6] font-CaveatBrush h-64 ps-2 mx-auto border-[3.5px] rounded-full flex items-center justify-center -rotate-[10deg] text-brand-orange bg-white w-[16.5rem] border-brand-orange">
+		Thank<br>you
+	</p>
+	<p class="text-center text-2xl mt-4">We’ll let you know when <span class="font-bold">Choreless</span> is Available in your area</p>
+</div>
+</template>

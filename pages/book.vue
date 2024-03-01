9d8c42bebcb9518a27a850941f0d54057ac8c5f8
imithu <imithu@imithu.org>
2024-03-01 06:52:29 +0600

    🌱 add - GettingStarted and ContactInformation component

--- pages/book.vue
+++ pages/book.vue
@@ -1,5 +1,7 @@
 <script setup lang="ts">
 import NotAvailableInYourArea from '@/components/book/NotAvailableInYourArea.vue';
+import GettingStarted from '@/components/book/GettingStarted.vue';
+import ContactInformation from '@/components/book/ContactInformation.vue';
 import Header from '@/components/book/Header.vue';
 import SelectServices from '@/components/book/SelectServices.vue';
 import CarePreferences from '@/components/book/CarePreferences.vue';
@@ -11,16 +13,22 @@ definePageMeta({
 })
 
 const book = usePageBook();
+const {query} = useRoute();
+if('usertype' in query && typeof query.usertype==='string' && query.usertype==='old') book.usertype = 'old';
 </script>
 
 <template>
 <div>
 	<NotAvailableInYourArea v-if="book.step===-1" />
+	<div v-else-if="book.usertype==='new' && book.step===0" class="min-h-[92vh] mx-auto flex items-center justify-center">
+		<GettingStarted />
+	</div>
 	<template v-else>
 		<Header />
-		<SelectServices v-if="book.step===0" />
-		<CarePreferences v-else-if="book.step===1" />
-		<SelectPickupInfo v-else-if="book.step===2" />
+		<ContactInformation v-if="book.usertype==='new' && book.step===1" />
+		<SelectServices v-else-if="book.step===(book.usertype==='new' ? 2 : 0)" />
+		<CarePreferences v-else-if="book.step===(book.usertype==='new' ? 3 : 1)" />
+		<SelectPickupInfo v-else-if="book.step===(book.usertype==='new' ? 4 : 2)" />
 	</template>
 </div>
 </template>

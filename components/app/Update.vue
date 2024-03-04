f1bfb190f2954858a6346990b8c67826222635ec
imithu <imithu@imithu.org>
2024-03-05 03:50:44 +0600

    🌱 update - Update component

--- components/app/Update.vue
+++ components/app/Update.vue
@@ -1,13 +1,7 @@
 <script setup lang="ts">
-import type { Changelog } from '~/types/misc';
+import Changelog from '~/components/app/Changelog.vue';
 
 const { $pwa } = useNuxtApp();
-const api = useApi();
-
-const changelog = ref<Changelog[]>([])
-
-const {data}: {data: Changelog[]} = await api.get('/changelog/admin');
-changelog.value = data;
 </script>
 
 <template>
@@ -18,12 +12,7 @@ changelog.value = data;
 	</button>
 	<h1 class="text-xl">Update is available.</h1>
 	<div class="w-full px-4">
-		<div class="max-w-lg max-h-40 w-full mx-auto px-2 flex flex-col gap-y-4 overflow-y-auto">
-			<div v-for="cl of changelog" :key="cl.version">
-				<p class="sticky top-0 text-lg font-bold bg-white text-success">v{{ cl.version }}</p>
-				<p v-for="(change, change_key) of cl.changes" :key="change_key">{{ change }}</p>
-			</div>
-		</div>
+		<Changelog class="max-w-lg max-h-40 mx-auto px-2" />
 	</div>
 </div>
 </template>

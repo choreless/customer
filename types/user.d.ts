f45ff28bb655dbc2d7aed8065929ba0c625b4fcb
imithu <imithu@imithu.org>
2024-03-07 16:11:27 +0600

    🔥 remove - CustomerInfo4Washer type

--- types/user.d.ts
+++ types/user.d.ts
@@ -1,10 +1,2 @@
 export type Role = 'admin' | 'customer' | 'driver' | 'washer' | '';
 export type CustomerType = 'retail' | 'commercial';
-
-export interface CustomerInfo4Washer {
-	detergent: string
-	water_temperature: string
-	dryer_temperature: string
-	comment: string|null
-	customer_name: string
-}

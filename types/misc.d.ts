dfb1679b60fbd9e98d35618e2873d245f0237f3d
imithu <imithu@imithu.org>
2024-03-22 03:21:58 +0600

    🏷️ add - InputStatus and InputStatusWithProgress type

--- types/misc.d.ts
+++ types/misc.d.ts
@@ -2,3 +2,7 @@ export interface Changelog {
 	version: string,
 	changes: string[]
 }
+
+// form
+export type InputStatus = undefined|boolean;
+export type InputStatusWithProgress = InputStatus|'progress';

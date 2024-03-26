804bd3241857c139d6bc2e528ba644de5d47bfbb
writeeasyjs <shuvo.devhub@gmail.com>
2024-03-26 11:32:45 +0600

    design: first row of summary info

--- pages/summary.vue
+++ pages/summary.vue
@@ -12,18 +12,24 @@
     </div>
     <div class="p-[164px] space-y-5">
         <img src="https://ik.imagekit.io/choreless/v2/icons/choreless.svg"/>
-        <div>
+        <div class="space-y-20">
             <div class="space-y-[10px]">
                 <h4 class="text-black text-[2rem] font-bold leading-[38px]">Summary</h4>
                 <small class=" text-sm text-[#838383] max-w-[322px]">Please double check all the information to ensure it is <br> correct before booking</small>
             </div>
             <div>
-                <div>
+                <div class="flex items-start gap-4">
                     <div class=" size-8 rounded-full items-center justify-center inline-flex shrink-0 bg-[#EDEEF1]">
                         <icon-coin fill="black" width="16" height="16"/>
                     </div>
                     <div>
-                        <span class=" text-sm">Due today</span>
+                        <span class=" text-sm text-[#838383]">Due today</span>
+                        <h4 class="text-black text-base font-medium">$35 minimum charge</h4>
+                        <div class="text-xs text-[#838383]">
+                            If order total exceeds minimum charge, 
+                            <br>you will be charged the difference.
+                            <nuxt-link to="#" v-text="'learn more'"/>
+                        </div>
                     </div>
                 </div>
             </div>

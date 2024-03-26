40753150babdac197365fd152799638e3f2f6353
writeeasyjs <shuvo.devhub@gmail.com>
2024-03-26 10:26:45 +0600

    fix: googlemaps api loader issue

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,34 +1,37 @@
 /** base */
-const APP_NAME = 'Customer';
-const APP_VERSION = process.env.APP_VERSION ?? 'Dev';
-const APP_URL = process.env.APP_URL ?? 'http://localhost:3000';
-const API_MAIN = process.env.API_MAIN ?? 'https://api.getchoreless.com';
-const GOOGLE_API_KEY = process.env.GOOGLE_API_KEY ?? '';
+const APP_NAME = "Customer";
+const APP_VERSION = process.env.APP_VERSION ?? "Dev";
+const APP_URL = process.env.APP_URL ?? "http://localhost:3000";
+const API_MAIN = process.env.API_MAIN ?? "https://api.getchoreless.com";
+const GOOGLE_API_KEY = process.env.GOOGLE_API_KEY ?? "";
 
 /** dev */
 const DEBUG = Boolean(process.env.DEBUG) ?? false;
 
 export default defineNuxtConfig({
-	modules: ['@nuxtjs/tailwindcss', '@pinia/nuxt', 'nuxt-icon', '@vueuse/nuxt'],
+  build: {
+    transpile: ["@googlemaps/js-api-loader"],
+  },
+  modules: ["@nuxtjs/tailwindcss", "@pinia/nuxt", "nuxt-icon", "@vueuse/nuxt"],
 
-	tailwindcss: {
-		viewer: false,
-		cssPath: '~/assets/css/index.css'
-	},
+  tailwindcss: {
+    viewer: false,
+    cssPath: "~/assets/css/index.css",
+  },
 
-	runtimeConfig: {
-		public: {
-			base: {
-				APP_NAME,
-				APP_VERSION,
-				APP_URL,
-				API_MAIN,
-				GOOGLE_API_KEY
-			},
-			dev: {
-				DEBUG
-			}
-		}
-	},
-	devtools: { enabled: DEBUG }
-})
+  runtimeConfig: {
+    public: {
+      base: {
+        APP_NAME,
+        APP_VERSION,
+        APP_URL,
+        API_MAIN,
+        GOOGLE_API_KEY,
+      },
+      dev: {
+        DEBUG,
+      },
+    },
+  },
+  devtools: { enabled: DEBUG },
+});

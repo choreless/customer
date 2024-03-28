94546ba1a903705fc84bfbdf6a6a044c406982cb
writeeasyjs <shuvo.devhub@gmail.com>
2024-03-28 21:04:00 +0600

    fix: promocode input width

--- components/form/promocode/Input.vue
+++ components/form/promocode/Input.vue
@@ -21,7 +21,8 @@ const btnText = computed(() => {
 </script>
 
 <template>
-    <div class="flex items-center py-[7px] px-2.5 rounded-[5px] border relative" :class="borderClass">
+    <div class="flex items-center py-[7px] px-2.5 rounded-[5px] border relative justify-between min-w-[320px]"
+        :class="borderClass">
         <input type="text" v-model="promoCode" placeholder="Enter promo code" class="outline-0 border-0 relative" />
         <button @click="$emit('applyPromoCode')" :disabled="!promoCode"
             class=" disabled:opacity-45 py-[5px] px-2.5 rounded-[5px] bg-brand-orange text-white hover:bg-orange-700 transition duration-200 ease-linear "

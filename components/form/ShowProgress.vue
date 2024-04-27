0a17d3d0912bdbd14f52300de1813028df21be67
imithu <imithu@imithu.org>
2024-04-28 01:17:37 +0600

    🌱 add - ShowProgress Component

---
+++ components/form/ShowProgress.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+defineProps<{
+	complete: number
+}>();
+</script>
+
+<template>
+<div class="w-full h-2 transition-transform duration-500" :style="{transform: `translateX(min(0%, min(100%, ${complete-100}%)))`}" />
+</template>

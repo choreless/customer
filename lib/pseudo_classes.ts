8296370a9211f8f3f7fe5224e595b68eed37f75a
imithu <imithu@imithu.org>
2024-05-12 14:00:30 +0600

    💄 add - .multiselect-dropdown-separate

--- lib/pseudo_classes.ts
+++ lib/pseudo_classes.ts
@@ -3,6 +3,7 @@ import plugin from 'tailwindcss/plugin';
 export default plugin(function({addComponents}){
 	addComponents({
 		'.input-float': {},
-		'.input-float2': {}
+		'.input-float2': {},
+		'.multiselect-dropdown-separate': {}
 	})
 });

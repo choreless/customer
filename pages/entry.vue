b949b1b406c7a11445ace4e20810b410690e25f4
imithu <imithu@imithu.org>
2024-05-31 15:55:54 +0600

    🌱 add - entry page

---
+++ pages/entry.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import Header from '@/components/entry/Header.vue';
+import GettingStarted from '@/components/entry/GettingStarted.vue';
+
+useHead({ title: 'Entry' })
+definePageMeta({
+	layout: 'clean'
+})
+
+const entry = usePageEntry();
+</script>
+
+<template>
+<div>
+	<Header />
+	<div class="mt-2.5">
+		<FormShowProgress class="bg-brand-orange" :value="entry.step/1" />
+	</div>
+	<GettingStarted v-if="entry.step===0" />
+</div>
+</template>

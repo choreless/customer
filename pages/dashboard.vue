fc2f9282fa65857400141e867058a1360a97c506
imithu <imithu@imithu.org>
2024-06-06 00:08:30 +0600

    🌱 update - clean dashboard page

--- pages/dashboard.vue
+++ pages/dashboard.vue
@@ -1,15 +1,10 @@
 <script setup lang="ts">
-import AdminPanel from '~/components/dashboard/AdminPanel.vue';
-import WasherPanel from '~/components/dashboard/WasherPanel.vue';
-
 useHead({ title: 'Dashboard' })
 definePageMeta({ middleware: 'auth' })
-const {usertype} = useUser();
 </script>
 
 <template>
 <div>
-	<AdminPanel v-if="usertype==='admin'" />
-	<WasherPanel v-if="usertype==='washer'" />
+	<p>Under Development</p>
 </div>
 </template>

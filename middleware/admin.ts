b5fb36e213850b1fa57a4dba5537fff524e3bd63
imithu <imithu@imithu.org>
2024-06-06 00:12:51 +0600

    🔥 remove - admin and washer middlewares

--- middleware/admin.ts
+++
@@ -1,6 +0,0 @@
-export default defineNuxtRouteMiddleware(() => {
-	const {usertype} = useUser();
-
-	if(!usertype) return navigateTo('/');
-	if(usertype!=='admin') return navigateTo('/dashboard');
-})
